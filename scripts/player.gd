9ca5dfe0e7c2d57d353e206d4d1dfccaa476e153
Nathan Cloutier <nathanc4571@gmail.com>
2026-03-07 13:00:55 -0500

    Add some super cool animations!

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 500
+const SPEED = 350
 const JUMP_VELOCITY = -700
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 func respawn():
@@ -13,7 +15,9 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		if velocity.y < 0:
+			animated_sprite.play("Jump")
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -23,7 +27,12 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
+		if is_on_floor():
+			animated_sprite.play("Run")
+		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			animated_sprite.play("Idle")
 
 	move_and_slide()
